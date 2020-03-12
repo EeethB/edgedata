ff45fe8d567fb4026443a003efe2793c0fbd0ab0
ArctiCondor <ethanbrockmann@gmail.com>
2020-03-11 17:12:56 -0700

    Added basic documentation

--- R/data.R
+++ R/data.R
@@ -1,6 +1,56 @@
-#' RA-eligible CPT/HCPCS
+#' RA-eligible CPT/HCPCS - Table 2
 #'
 #'
 #'@source \url{https://www.cms.gov/cciio/Resources/Regulations-and-Guidance/index}
 
-"tbl2"
+"cpt_hcpcs"
+
+
+#' ICD to Condition Category Crosswalk - Table 3
+#'
+#'
+#'@source \url{https://www.cms.gov/cciio/Resources/Regulations-and-Guidance/index}
+
+"icd_cc"
+
+
+#' Condition Category Hierarchies - Table 4
+#'
+#'
+#'@source \url{https://www.cms.gov/cciio/Resources/Regulations-and-Guidance/index}
+
+"cc_hier"
+
+
+#' Model Coefficients - Table 9
+#'
+#'
+#'@source \url{https://www.cms.gov/cciio/Resources/Regulations-and-Guidance/index}
+
+"coefs"
+
+
+#' NDC to Rx Condition Crosswalk - Table 10a
+#'
+#'
+#'@source \url{https://www.cms.gov/cciio/Resources/Regulations-and-Guidance/index}
+
+"ndc_rxc"
+
+
+
+#' HCPCS to Rx Condition Crosswalk - Table 10b
+#'
+#'
+#'@source \url{https://www.cms.gov/cciio/Resources/Regulations-and-Guidance/index}
+
+"hcpcs_rxc"
+
+
+
+#' Rx Condition Hierarchies - Table 11
+#'
+#'
+#'@source \url{https://www.cms.gov/cciio/Resources/Regulations-and-Guidance/index}
+
+"rxc_hier"
