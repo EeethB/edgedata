9e05cac4285388ae701a8ca94a02dfa80d8812a2
ArctiCondor <ethanbrockmann@gmail.com>
2020-03-11 17:03:40 -0700

    Added RXC hierarchy (Table 11)

--- data-raw/hcpcs_rxc.R
+++ data-raw/hcpcs_rxc.R
@@ -4,21 +4,21 @@ library(stringr)
 
 source("./data-raw/tables_path.R")
 
-## Table 10a
-tbl10a <- read_excel(
+## Table 10b
+tbl10b <- read_excel(
   tbl_path,
-  sheet = "Table 10a",
-  range = "A4:C15000",
+  sheet = "Table 10b",
+  range = "A4:C100",
   col_types = c("text", "text", "text")
 )
 
-colnames(tbl10a) <- c(
+colnames(tbl10b) <- c(
   "rxc",
   "desc",
-  "ndc"
+  "hcpcs"
 )
 
-ndc_rxc <- tbl10a %>%
+ndc_rxc <- tbl10b %>%
   filter(str_length(rxc) <= 3)
 
 usethis::use_data(ndc_rxc, overwrite = TRUE)
---
+++ data-raw/rxc_hier.R
@@ -0,0 +1,36 @@
+library(readr)
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(readxl)
+
+source("./data-raw/tables_path.R")
+
+## Table 11
+tbl11 <- read_excel(
+  tbl_path,
+  sheet = "Table 11",
+  range = "A3:C50"
+)
+
+colnames(tbl11) <- c(
+  "rxc",
+  "set_0",
+  "desc"
+)
+
+rxc_hier <- tbl11 %>%
+  separate(
+    col = set_0,
+    into = paste0("cond", 1:5),
+    sep = " ,",
+    fill = "right"
+  ) %>%
+  pivot_longer(
+    cols = starts_with("cond"),
+    values_to = "set_0"
+  ) %>%
+  filter(!is.na(set_0)) %>%
+  select(-name)
+
+usethis::use_data(rxc_hier)
