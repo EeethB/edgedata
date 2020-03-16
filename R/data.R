83ac258490260132a168c2d92ab0935642352499
ArctiCondor <ethanbrockmann@gmail.com>
2020-03-16 08:37:52 -0700

    Updated docs and worked on coefs tables

--- R/data.R
+++ R/data.R
@@ -5,6 +5,7 @@
 #' codes that determine if a claim is eligible for risk
 #' adjustment.
 #'
+#' @format
 #' \describe{
 #'   \item{code}{CPT/HCPCS code}
 #'   \item{desc}{Short description of the code}
@@ -23,13 +24,15 @@
 #' A dataset containing the ICD-10 to condition category
 #' mappings.
 #'
+#' @format
 #' \describe{
 #'   \item{icd}{ICD-10 code}
 #'   \item{cc}{Risk adjustment condition category}
 #'   \item{class}{It's not yet clear how this is used}
 #'   \item{eff_date}{Effective date of ICD code}
 #'   \item{term_date}{Term date of ICD code}
-#'   \item{sex}{Male/Female specification for ICD code (Risk adjustment currently only supports male and female genders)}
+#'   \item{sex}{Male/Female specification for ICD code (Risk adjustment
+#'              currently only supports male and female)}
 #'   \item{sex_num}{Numeric sex encoding (1/M and 2/F)}
 #' }
 #'
@@ -40,8 +43,18 @@
 
 #' Condition Category Hierarchies - Table 4
 #'
-#' A dataset used to apply the risk adjustment condition
-#' category hierarchies.
+#' A dataset used to apply the risk adjustment condition category hierarchies.
+#' The HHS risk model uses *hierarchichal* condition categories. This
+#' means that when an individual has e.g. HCC 8 (Metastatic Cancer), they do
+#' not also get the risk score for less serious cancer categories.
+#'
+#' @format
+#' \describe{
+#'   \item{cc}{HHS condition category code}
+#'   \item{desc}{HHS condition category description}
+#'   \item{set_0}{HHS condition category that falls below `cc` on the
+#'                hierarchy}
+#' }
 #'
 #' @source
 #' \url{https://www.cms.gov/cciio/Resources/Regulations-and-Guidance/index}
@@ -51,6 +64,18 @@
 
 #' Model Coefficients - Table 9
 #'
+#' A dataset containing the risk adjustment coefficients. Risk varies by sex,
+#' age, enrollment duration, condition category, prescription drug category,
+#' and adult/child/infant status. There are also some drug/diagnosis
+#' interaction terms, and some "high risk" interaction terms.
+#'
+#' @format
+#' \describe{
+#'   \item{model}{Adult, Child, or Infant model, which all have different
+#'                variables and different coefficients}
+#'   \item{var}{Name of the variable in the CMS DIY software. This variable
+#'              encodes every direction that risk can vary}
+#' }
 #'
 #' @source \url{https://www.cms.gov/cciio/Resources/Regulations-and-Guidance/index}
 
--- data-raw/9_coefs.R
+++ data-raw/9_coefs.R
@@ -1,5 +1,7 @@
 library(dplyr)
 library(readxl)
+library(stringr)
+library(tidyr)
 
 source("./data-raw/tables_path.R")
 
@@ -21,7 +23,94 @@ colnames(tbl9) <- c(
   "cat"
 )
 
-coefs <- tbl9 %>%
-  filter(!is.na(model))
+tbl9_grp <- tbl9 %>%
+  filter(!is.na(model)) %>%
+  mutate(
+    var_type = case_when(
+      str_detect(var, "G[0-9]{1,2}") ~ "group",
+      str_detect(var, "([MF]AGE_LAST)|(AGE[0-1])") ~ "demo",
+      str_detect(var, "HHS_HCC") ~ "hcc",
+      str_detect(var, "INT_GROUP_") ~ "interaction",
+      str_detect(var, "ED_") ~ "enroll_dur",
+      str_detect(var, "RXC_[0-9]{1,2}_X") ~ "rxc_hcc_inter",
+      str_detect(var, "RXC_[0-9]{1,2}") ~ "rxc",
+      str_detect(var, "X_SEVERITY") ~ "inf",
+      TRUE ~ "other"
+    )
+  ) %>%
+  group_by(model, var_type)
 
-usethis::use_data(coefs, overwrite = TRUE)
+group_list <- group_split(tbl9_grp)
+
+group_names <- group_keys(tbl9_grp) %>%
+  mutate(name = paste(tolower(model), var_type, "", sep = "_")) %>%
+  select(name)
+
+names(group_list) <- group_names[[1]]
+
+attach(group_list)
+
+adult_demo <- adult_demo_ %>%
+  mutate(
+    sex = str_sub(var, 1, 1),
+    age_min = str_sub(var, 11, 12),
+    age_max = str_sub(str_replace(var, "GT", "999"), 14)
+  ) %>%
+  select(-model, -var, -var_type)
+
+adult_enroll_dur <- adult_enroll_dur_ %>%
+  mutate(months = str_sub(var, 4,)) %>%
+  select(-model, -var, -var_type)
+
+adult_group <- adult_group_ %>%
+  select(-model, -var_type)
+
+adult_hcc <- adult_hcc_ %>%
+  mutate(hcc = str_sub(var, 8)) %>%
+  select(-model, -var, -var_type)
+
+adult_interaction <- adult_interaction_ %>%
+  select(-model, -var_type)
+
+adult_rxc <- adult_rxc_ %>%
+  mutate(rxc = str_sub(var, 5)) %>%
+  select(-model, -var, -var_type)
+
+adult_rxc_hcc_inter <- adult_rxc_hcc_inter_ %>%
+  mutate(
+    rxc = str_sub(var, 5, 6),
+    hcc_list = str_sub(var, 13) %>% str_replace("37_", "37")
+  ) %>%
+  separate(
+    col = hcc_list,
+    into = paste0("hcc", 1:5),
+    sep = "_",
+    fill = "right"
+  ) %>%
+  pivot_longer(
+    cols = starts_with("hcc"),
+    values_to = "hcc"
+  ) %>%
+  filter(!is.na(hcc)) %>%
+
+
+  cc_hier <- tbl4 %>%
+  separate(
+    col = set_0,
+    into = paste0("cond", 1:10),
+    sep = " ,",
+    fill = "right"
+  ) %>%
+  pivot_longer(
+    cols = starts_with("cond"),
+    values_to = "set_0"
+  ) %>%
+  filter(!is.na(set_0)) %>%
+  select(-name)
+
+adult_rxc_hcc_inter
+
+group_names
+
+usethis::use_data(adult_demo, adult_enroll_dur, adult_grup, adult_hcc,
+                  adult_interaction, adult_rxc)
